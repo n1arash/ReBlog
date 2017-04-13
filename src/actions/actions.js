d7be2f7e7c777073c2d6126841bcdb16bd186a49
Arash Khazaei <0xclay@gmail.com>
2017-04-14 01:19:44 +0430

    add editPost action creator & type checking for addPost

--- src/actions/actions.js
+++ src/actions/actions.js
@@ -1,10 +1,27 @@
 // @flow
 import * as actionTypes from './actionTypes';
 
-export const addPost = (title:string,content:string) : Object => {
-  return {
-    type: actionTypes.ADD_POST,
-    title,
-    content
+export const addPost = (title:string,content:string) : ?Object => {
+  if(title !== '' && content !== '' && typeof title === 'string' && typeof content === 'string'){
+    return {
+      type: actionTypes.ADD_POST,
+      title,
+      content
+    }
   }
+   return ;
+}
+
+export const editPost = (id:number,title:string,content:string) : ?Object => {
+  if(typeof title === 'string' && typeof id === 'number' && typeof content === 'string' ){
+    if(title !== '' && content !== '' && id !== ''){
+      return {
+        type : actionTypes.EDIT_POST,
+        id,
+        title,
+        content
+      }
+    }
+  }
+  retrun ;
 }
