f08e3750773b86d5f2fe3858433d58f1c8c99213
Arash Khazaei <0xclay@gmail.com>
2017-04-14 01:17:30 +0430

    delete usless dependencies

--- __tests__/App.test.js
+++ __tests__/App.test.js
@@ -1,9 +1,9 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import App from '../src/App';
 import { createStore } from 'redux';
+import App from '../src/App';
 import BlogApp from '../src/Reducers/BlogApp';
-import { Provider, connect } from 'react-redux';
+import { Provider } from 'react-redux';
 
 it('renders without crashing', () => {
   const div = document.createElement('div');
