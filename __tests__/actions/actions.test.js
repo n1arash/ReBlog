dd023ed345d2a6ca845c019a2f3ef727058cf646
Arash Khazaei <0xclay@gmail.com>
2017-04-14 06:06:25 +0430

    test for DELETE_POST,GET_POST & EDIT_POST Action Creators

--- __tests__/actions/actions.test.js
+++ __tests__/actions/actions.test.js
@@ -1,7 +1,7 @@
-import {addPost,editPost} from '../../src/actions/actions';
+import {addPost,editPost,deletePost,getPosts} from '../../src/actions/actions';
 
 
-describe('ADD_POST Action !', ( ) => {
+describe('ADD_POST Action Creator !', ( ) => {
     it('should return post Object', ()=>{
         let Post = { type: 'ADD_POST', title: 'TEST', content:'TEST' }
         expect(addPost('TEST','TEST')).toEqual(Post)
@@ -12,7 +12,7 @@ describe('ADD_POST Action !', ( ) => {
     })
 })
 
-describe('EDIT_POST Action',()=>{
+describe('EDIT_POST Action Creator',()=>{
     it('should return EDITED POST',() => {
         expect(editPost(1,'EDITED','EDITED')).toEqual({
             type:'EDIT_POST',
@@ -27,4 +27,33 @@ describe('EDIT_POST Action',()=>{
             content:'TEST'
         })
     })
+    it('should return undefined',()=>{
+      expect(editPost("a", [],{})).toBe(undefined)
+      expect(editPost(1, 2, 4)).toBe(undefined)
+      expect(editPost(null, null, undefined)).toBe(undefined)
+    })
+
+})
+
+describe('DELETE_POST Action Creator',() => {
+  it('should DELETE a postById',()=> {
+    expect(deletePost(1)).toEqual({
+      type: 'DELETE_POST',
+      id: 1
+    })
+  })
+  it('should return undefined',()=>{
+    expect(deletePost('id')).toBe(undefined)
+    expect(deletePost(null)).toBe(undefined)
+    expect(deletePost()).toBe(undefined)
+    expect(deletePost([])).toBe(undefined)
+    expect(deletePost({})).toBe(undefined)
+  })
+})
+
+
+describe('GET_POST Action Creator', () =>{
+  it('should return a Object', ()=>{
+    expect(getPosts()).toEqual({type:'GET_POST'})
+  })
 })
