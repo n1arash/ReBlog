112f41343f99e1c12a1de911041e36668a4a5eeb
Arash Khazaei <0xclay@gmail.com>
2017-04-14 06:05:32 +0430

    test for DELETE_POST & GET_POST

--- __tests__/Reducers/PostReducer.test.js
+++ __tests__/Reducers/PostReducer.test.js
@@ -27,7 +27,7 @@ describe('ADD_POST Action', () => {
 describe('EDIT_POST Action',() =>{
   const date = new Date().toUTCString()
   it('should edit POST', () => {
-    
+
     let state = {
       type: 'EDIT_POST',
       id: 1,
@@ -71,3 +71,85 @@ describe('EDIT_POST Action',() =>{
     expect(PostsReducer(initialState, state)).toEqual(initialState)
   })
 })
+
+describe('DELETE_POST Action', () => {
+    const date = new Date().toUTCString()
+    it('should DELETE a POST', () => {
+      let state = [
+        {
+        id: 1,
+        title : "TEST",
+        content : 'LOREM',
+        date
+      },
+      {
+        id:2 ,
+        title:"TEST2",
+        content : "LOREM",
+        date
+      }
+    ]
+        expect(PostsReducer(state, {type : 'DELETE_POST', id : 2})).toEqual([
+        {
+          id:1,
+          title:"TEST",
+          content: "LOREM",
+          date
+        }
+      ])
+        expect(PostsReducer(state, {type : 'DELETE_POST', id : 1})).toEqual([
+        {
+          id:2,
+          title:"TEST2",
+          content: "LOREM",
+          date
+        }
+      ])
+    })
+    it('should return initialState', () => {
+      expect(PostsReducer([{id:1,title:'X',content:'X',date}], {type:'DELETE_POST',id:2})).toEqual([
+        {
+        id:1,
+        title:'X',
+        content:'X',
+        date
+        }
+       ])
+    })
+})
+
+describe('GET_POST Action',() => {
+  const date = new Date().toUTCString();
+  it('should return all posts',() => {
+    let initialState = [
+      {
+        id:1,
+        title:'TEST',
+        content:'Lorem',
+        date
+      },
+      {
+        id:2,
+        title:'TEST2',
+        content:'Lorem',
+        date
+      },
+      {
+        id:3,
+        title:'TEST3',
+        content:'Lorem',
+        date
+      },
+      {
+        id:4,
+        title:'TEST4',
+        content:'Lorem',
+        date
+      }
+    ]
+    expect(PostsReducer(initialState, {type: 'GET_POST'})).toEqual(initialState)
+  })
+  it('should return empty array',() => {
+    expect(PostsReducer([], {type:'GET_POST'})).toEqual([])
+  })
+});
